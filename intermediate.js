b5422ebcd154fae048074125038c26ce7d078ab4
Ismael P. Paiva <ismael.paiva@fatec.sp.gov.br>
2018-11-04 15:12:58 -0200

    ALMOST DONE

--- intermediate.js
+++ intermediate.js
@@ -2,22 +2,43 @@
 // and returns an array of only the odd numbers
 // ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
 function returnOdds(array) {
+  const odds = array.filter(function (valor) {
+    return valor % 2 !== 0;
+  });
+  return odds;
 }
+returnOdds([1, 2, 3, 4, 5, 6, 7]);
 
 // this function accepts an array of numbers
 // and returns an array of only the even numbers
 // ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
 function returnEvens(array) {
+  const evens = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 2 !== 1) {
+     evens.push(array[i]);
+    }
+    return evens;
 }
+returnEvent([1, 2, 3, 4, 5, 6, 7]);
 
 // returns only the max element from the inputted array of numbers
 // ex: findMax([1,25,6,3]); -> 25
 function findMax(array) {
+  let max = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  } return max;
 }
-
+findMax([1, 25, 6, 3]);
 /**
  * remove leading and trailing whitespace or specified characters from string
  * trim(' hello '); -> 'hello'
  */
 function trim(string) {
+  return string.trim();
 }
+trim(' hello ');
+
