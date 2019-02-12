865262aa0564a9220fdd0c76614f19f2d91626c6
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-02-12 00:20:16 -0200

    First Commit. Advanced to be deliveded Tomorrow (12/02)

--- basic.js
+++ basic.js
@@ -2,53 +2,53 @@
 // ex: addTwo(5); -> 7
 // ex: addTwo(6); -> 8
 function addTwo(number) {
-
+  return number + 2;
 }
 
 // adds the two inputted numbers together
 // ex: add(5, 10); -> 15
 function add(num1, num2) {
-
+  return num1 + num2;
 }
 
 // adds all three inputted numbers together
 // ex: addd(1,2,3); -> 6
 // ex: addd(2,4,2); -> 8
 function addd(value1, value2, value3) {
-
+  return value1 + value2 + value3;
 }
 
 // subtracts the second inputted number from the first inputted number
 // ex: subtract(5, 10); -> -5
 function subtract(n1, n2) {
-
+  return n1 - n2;
 }
 
 // multiplies the inputted number by 2
 // ex: multiplyByTwo(5); -> 10
 // ex: multiplyByTwo(6); -> 12
 function multiplyByTwo(numero) {
-
+  return numero * 2;
 }
 
 // multplies the two inputted numbers together
 // ex: multiply(1,2); -> 2
 // ex: multiply(2,2); -> 4
 function multiply(val1, val2) {
-
+  return val1 * val2;
 }
 
 // multiplies all three inputted numbers together
 // ex: multiplyy(1,2,3); -> 6
 // ex: multiplyy(2,2,3); -> 12
 function multiplyy(a, b, c) {
-
+  return a * b * c;
 }
 
 // returns the square of the inputted number
 // ex: square(5); -> 25
 function square(value) {
-
+  return Math.pow(value, 2);
 }
 
 // determines if the number is odd
@@ -57,7 +57,10 @@ function square(value) {
 // ex: isOdd(4); -> true
 // ex: isOdd(5); -> false
 function isOdd(num) {
-
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
 }
 
 // determines if the number is negative
@@ -67,40 +70,55 @@ function isOdd(num) {
 // ex: isNegative(1); -> false
 // ex: isNegative(0); -> false
 function isNegative(num) {
-
+  if (num < 0) {
+    return true;
+  }
+  return false;
 }
 
 // returns the absolute value of the inputted number
 // ex: positive(-1); -> 1
 // ex: positive(1); -> 1
 function positive(num) {
-
+  return Math.abs(num);
 }
 
 // Returns boolean of whether argument is classified as a Number object
 // isNumber(5); → true
 // isNumber('hi'); → false
 function isNumber(value) {
-
+  if (isNaN(value)) {
+    return false;
+  }
+  return true;
 }
 
 // Returns boolean of whether argument is classified as an Array object
 // isArray(5); → false
 // isArray([1,2,3]); → true
 function isArray(value) {
-
+  if (Array.isArray(value)) {
+    return true;
+  }
+  return false;
 }
 
 // Returns boolean of whether argument is classified as an Object
 // isObject(5); → false
 // isObject([1,2,3]); → true
 function isObject(value) {
-
+  if (value instanceof Object) {
+    return true;
+  }
+  return false;
 }
 
 // return boolean of whether argument is classified as null
 // isNull(null); -> true
 // isNull(5); -> false
 function isNull(value) {
-
+  if (value === null) {
+    return true;
+  }
+  return false;
 }
--- intermediate.js
+++ intermediate.js
@@ -2,17 +2,33 @@
 // and returns an array of only the odd numbers
 // ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
 function returnOdds(array) {
+  const result = [];
+  array.forEach((element) => {
+    if (element % 2 === 0) {
+      result.push(element);
+    }
+  });
+  return result;
 }
 
 // this function accepts an array of numbers
 // and returns an array of only the even numbers
 // ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
 function returnEvens(array) {
+  const result = [];
+  array.forEach((element) => {
+    if (element % 2 !== 0) {
+      result.push(element);
+    }
+  });
+  return result;
 }
 
 // returns only the max element from the inputted array of numbers
 // ex: findMax([1,25,6,3]); -> 25
 function findMax(array) {
+  array.sort((a, b) => a - b);
+  return array[array.length - 1];
 }
 
 /**
@@ -20,4 +36,5 @@ function findMax(array) {
  * trim(' hello '); -> 'hello'
  */
 function trim(string) {
+  return string.trim();
 }
